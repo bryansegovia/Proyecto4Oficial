9a98642b47743f53323adb43cf55afd612ec5527
bryansegovia <bsegovia@espol.edu.ec>
2023-06-19 09:01:59 -0500

    Todo guchi

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -6,8 +6,13 @@ const funcionPredeterminada = () => {
 
         const valorPorHora = parseFloat(document.getElementById('valorPorHora').value);
         const interfazHoras = parseFloat(document.getElementById('interfazHoras').value);
+        const horasHtml = parseFloat(document.getElementById('horasHtml').value);
+        const horasCSS = parseFloat(document.getElementById('horasCSS').value);
+        const horasTesting = parseFloat(document.getElementById('horasTesting').value);
+        const horasAPI = parseFloat(document.getElementById('horasAPI').value);
+        const horasServidor = parseFloat(document.getElementById('horasServidor').value);
 
-        let resultado = valorPorHora * interfazHoras;
+        let resultado = valorPorHora * (interfazHoras + horasHtml + horasCSS + horasTesting + horasAPI + horasServidor);
         document.getElementById('valorTotal').value = resultado;
     });
 };
